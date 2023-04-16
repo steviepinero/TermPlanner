f30371601e0459e1af10730e9ab438041b226137
stevie <steviepinero@gmail.com>
2023-04-16 14:02:44 -0400

    init commit

---
+++ app/src/main/java/com/wgu/termplanner/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wgu.termplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void newTerm(View view) {
+
+    }
+}
